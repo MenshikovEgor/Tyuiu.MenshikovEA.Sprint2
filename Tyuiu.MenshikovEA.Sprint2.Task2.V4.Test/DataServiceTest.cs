c4accd5c3992cf1eb172fc00c6b6e6edfd257332
Егор Меньшиков <Tyuiu.MenshikovEA@outlook.com>
2023-10-04 20:40:52 +0500

    Task2

---
+++ Tyuiu.MenshikovEA.Sprint2.Task2.V4.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MenshikovEA.Sprint2.Task2.V4.Lib;
+
+namespace Tyuiu.MenshikovEA.Sprint2.Task2.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 8;
+            int y = 1;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            Assert.AreEqual(res, false);
+        }
+    }
+}
---
+++ Tyuiu.MenshikovEA.Sprint2.Task2.V4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MenshikovEA.Sprint2.Task2.V4.Lib;
+
+namespace Tyuiu.MenshikovEA.Sprint2.Task2.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x;
+            int y;
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(" Введите значение переменной х: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Введите значение переменной y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            if (res == true)
+                Console.WriteLine("Точка с данными координатами является заштрихованной");
+            else
+                Console.WriteLine("Точка с данными координатами не является заштрихованной");
+            Console.ReadKey();
+        }
+    }
+}
